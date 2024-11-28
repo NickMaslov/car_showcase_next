2ec6e15e2f5e72cd82272ca393027550b48c38ae
Nick Maslov <kategorychnost@gmail.com>
2024-11-28 12:11:23 -0500

    created car catalogue filter bar

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import NavBar from "./NavBar";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SeachManufacturer from "./SeachManufacturer";
 
-export { Hero, CustomButton, NavBar, Footer };
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  CustomFilter,
+  SearchBar,
+  SeachManufacturer,
+};
