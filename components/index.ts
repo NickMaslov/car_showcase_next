03094b9d137eabb1d74632441d8e323d82181da9
Nick Maslov <kategorychnost@gmail.com>
2024-11-28 10:14:02 -0500

    created layout , Header and dynamic Footer

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,6 @@
 import CustomButton from "./CustomButton";
+import Footer from "./Footer";
 import Hero from "./Hero";
+import NavBar from "./NavBar";
 
-export default {
-  Hero,
-  CustomButton,
-};
+export { Hero, CustomButton, NavBar, Footer };
