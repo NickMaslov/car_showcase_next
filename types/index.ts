903a92822a42f9cc879096f78a4c7b46ba3f9637
Nick Maslov <kategorychnost@gmail.com>
2025-02-10 16:53:42 -0500

    added types

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,22 @@
 import { MouseEventHandler } from "react";
 
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export type CarState = CarProps[] & { message?: string };
+
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
